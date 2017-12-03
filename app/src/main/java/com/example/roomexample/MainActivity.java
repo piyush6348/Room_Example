7648cb9dab5d4754465e15b5a9bdd469be2a7b90
piyush6348 <piyush6348@gmail.com>
2017-12-03 21:48:46 +0530

    fetched db data into rv

--- app/src/main/java/com/example/roomexample/MainActivity.java
+++ app/src/main/java/com/example/roomexample/MainActivity.java
@@ -2,18 +2,36 @@ package com.example.roomexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.example.roomexample.adapter.UserAdapter;
 import com.example.roomexample.db.AppDatabase;
+import com.example.roomexample.db.User;
 import com.example.roomexample.utils.DatabaseInitializer;
+import com.example.roomexample.utils.RoomQueryListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView rvUsers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseInitializer.populateAsync(AppDatabase.getDatabase(this));
+        rvUsers = (RecyclerView) findViewById(R.id.rv_users);
+        rvUsers.setLayoutManager(new LinearLayoutManager(this));
+
+        DatabaseInitializer.populateAsync(AppDatabase.getDatabase(this),
+                new RoomQueryListener() {
+                    @Override
+                    public void onSuccess(List<User> userList) {
+                        UserAdapter userAdapter = new UserAdapter(MainActivity.this,userList);
+                        rvUsers.setAdapter(userAdapter);
+                    }
+                });
     }
 
     @Override

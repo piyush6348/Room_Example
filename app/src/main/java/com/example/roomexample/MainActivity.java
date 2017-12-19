e6715624b1438c1ba648e6002c2f48423e16ecd6
piyush6348 <piyush6348@gmail.com>
2017-12-20 03:01:32 +0530

    activity connection added

--- app/src/main/java/com/example/roomexample/MainActivity.java
+++ app/src/main/java/com/example/roomexample/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.roomexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.roomexample.db.AppDatabase;
+import com.example.roomexample.utils.DatabaseInitializer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseInitializer.populateAsync(AppDatabase.getDatabase(this));
+    }
+
+    @Override
+    protected void onDestroy() {
+        AppDatabase.destroyInstance();
+        super.onDestroy();
     }
 }
